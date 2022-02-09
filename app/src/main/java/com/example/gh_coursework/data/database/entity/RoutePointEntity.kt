83b823afce0040e1186da54dfdffccd49b038dbd
Bogdan Boiko <bogdanboiko19996@gmail.com>
2022-02-09 21:24:40 +0300

    Add room annotations to entities, fix gradle url property value

--- app/src/main/java/com/example/gh_coursework/data/database/entity/RoutePointEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/entity/RoutePointEntity.kt
@@ -1,7 +1,14 @@
 package com.example.gh_coursework.data.database.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(tableName = "routes_points", primaryKeys = ["routeId", "pointId"])
 data class RoutePointEntity(
+    @ColumnInfo(name = "routeId")
     val routeId: Int,
+    @ColumnInfo(name = "pointId")
     val pointId: Int,
+    @ColumnInfo(name = "position")
     val position: Int
 )
