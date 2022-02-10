48c55989d2be97d78f24ab86a7cde782db0ae9cd
Bogdan Boiko <bogdanboiko19996@gmail.com>
2022-02-10 16:13:25 +0300

    Implement navigation through checkbox trigger, add mapbox access token to string res, create fragments models

---
+++ app/src/main/java/com/example/gh_coursework/ui/private_point/PrivatePointsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.gh_coursework.ui.private_point
+
+import androidx.fragment.app.Fragment
+import com.example.gh_coursework.R
+
+class PrivatePointsFragment : Fragment(R.layout.private_points_fragment)
