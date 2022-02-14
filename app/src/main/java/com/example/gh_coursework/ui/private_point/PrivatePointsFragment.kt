91373a6923a06e9e4d7821714e68b5fb24ecb6fc
Bogdan Bojko <bogdanboiko19996@gmail.com>
2022-02-14 19:50:41 +0200

    /feature_private_points add map to fragment, add point setting

--- app/src/main/java/com/example/gh_coursework/ui/private_point/PrivatePointsFragment.kt
+++ app/src/main/java/com/example/gh_coursework/ui/private_point/PrivatePointsFragment.kt
@@ -1,6 +1,96 @@
 package com.example.gh_coursework.ui.private_point
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import com.example.gh_coursework.R
+import com.example.gh_coursework.databinding.PrivatePointsFragmentBinding
+import com.example.gh_coursework.ui.helper.convertDrawableToBitmap
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.AnnotationPlugin
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.OnMapClickListener
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 
-class PrivatePointsFragment : Fragment(R.layout.private_points_fragment)
+class PrivatePointsFragment : Fragment(R.layout.private_points_fragment) {
+    private var mapboxMap: MapboxMap? = null
+    private lateinit var binding: PrivatePointsFragmentBinding
+    private var mapView: MapView? = null
+    private var annotationApi: AnnotationPlugin? = null
+    private var pointAnnotationManager: PointAnnotationManager? = null
+    private val onMapClickListener = OnMapClickListener { point ->
+        addAnnotationToMap(point)
+        return@OnMapClickListener true
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = PrivatePointsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configMap()
+        view.viewTreeObserver.addOnGlobalLayoutListener {
+            configButton(view.width / 2f, view.height / 2f)
+        }
+    }
+
+    private fun configButton(width: Float, height: Float) {
+        val downTime = SystemClock.uptimeMillis()
+        val eventTime = SystemClock.uptimeMillis() + 100
+        val downAction = MotionEvent.obtain(
+            downTime, eventTime, MotionEvent.ACTION_DOWN,
+            width, height, 0)
+        val upAction = MotionEvent.obtain(
+            downTime, eventTime, MotionEvent.ACTION_UP,
+            width, height, 0)
+        binding.addPointButton.setOnClickListener {
+            binding.mapView.dispatchTouchEvent(downAction)
+            binding.mapView.dispatchTouchEvent(upAction)
+        }
+    }
+
+    private fun configMap() {
+        mapView = binding.mapView
+        annotationApi = mapView?.annotations
+        pointAnnotationManager = annotationApi?.createPointAnnotationManager()
+        mapboxMap = mapView?.getMapboxMap()?.also {
+            it.loadStyleUri(Style.MAPBOX_STREETS)
+            it.addOnMapClickListener(onMapClickListener)
+        }
+    }
+
+    private fun addAnnotationToMap(point: Point) {
+        activity?.applicationContext?.let {
+            bitmapFromDrawableRes(it, R.drawable.ic_def_point)?.let { image ->
+                pointAnnotationManager?.create(createAnnotationPoint(image, point))
+            }
+        }
+    }
+
+    private fun createAnnotationPoint(bitmap: Bitmap, point: Point): PointAnnotationOptions {
+        return PointAnnotationOptions()
+            .withPoint(point)
+            .withIconImage(bitmap)
+    }
+
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+}
