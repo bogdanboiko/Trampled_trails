a8bab0dfe760423ba8f37a247d894b463ba41530
Pavel Shakin <pashkin302@gmail.com>
2022-02-18 20:41:12 +0200

    Added location permissions ask and set up navigation to PrivateRoutesFragment.kt

--- app/src/main/java/com/example/gh_coursework/MainActivity.kt
+++ app/src/main/java/com/example/gh_coursework/MainActivity.kt
@@ -1,34 +1,53 @@
 package com.example.gh_coursework
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import com.example.gh_coursework.databinding.ActivityMainBinding
 import com.example.gh_coursework.ui.private_point.PrivatePointsFragmentDirections
-import com.example.gh_coursework.ui.private_route.PrivateRoutesFragmentDirections
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener {
+
     lateinit var binding: ActivityMainBinding
+    private val permissionsManager = PermissionsManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         configCheckBox()
+
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            requestStoragePermission()
+        } else {
+            permissionsManager.requestLocationPermissions(this)
+        }
     }
 
     private fun configCheckBox() {
         binding.checkboxRoutePlace.setOnCheckedChangeListener { _, b ->
+
             val navController = binding.navHostFragmentActivityMain.findNavController()
 
             if (b) {
-                binding.navHostFragmentActivityMain.findNavController()
+                navController
                     .navigate(
                         PrivatePointsFragmentDirections
                             .actionPrivatePointsFragmentToPrivateRoutesFragment()
                     )
             } else {
-                navController.popBackStack()
+                navController
+                    .navigate(
+                        PrivatePointsFragmentDirections
+                            .actionPrivatePointsFragmentToPrivateRoutesFragment()
+                    )
             }
         }
     }
@@ -40,6 +59,51 @@ class MainActivity : AppCompatActivity() {
             } else {
                 checkboxRoutePlace.isChecked = !checkboxRoutePlace.isChecked
             }
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(
+            this,
+            "This app needs location and storage permissions in order to show its functionality.",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            requestStoragePermission()
+        } else {
+            Toast.makeText(
+                this,
+                "You didn't grant the permissions required to use the app",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun requestStoragePermission() {
+        val permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        val permissionsNeeded: MutableList<String> = ArrayList()
+        if (
+            ContextCompat.checkSelfPermission(this, permission) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsNeeded.add(permission)
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsNeeded.toTypedArray(),
+                10
+            )
         }
     }
 }
