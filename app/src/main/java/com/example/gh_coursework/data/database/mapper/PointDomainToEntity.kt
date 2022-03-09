a8d425511c0b1478a731e265d90d07bf6044beee
bogdanboiko <bogdanboiko19996@gmail.com>
2022-03-09 15:53:55 +0200

    /feature_point_db fix mappers

--- app/src/main/java/com/example/gh_coursework/data/database/mapper/PointDomainToEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/mapper/PointDomainToEntity.kt
@@ -2,9 +2,7 @@ package com.example.gh_coursework.data.database.mapper
 
 import com.example.gh_coursework.data.database.entity.PointCoordinatesEntity
 import com.example.gh_coursework.domain.entity.PointPreviewDomain
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
-fun mapPointDomainToEntity(point: Flow<PointPreviewDomain>): Flow<PointCoordinatesEntity> {
-    return point.map { PointCoordinatesEntity(it.pointId, it.x, it.y) }
+fun mapPointDomainToEntity(point: PointPreviewDomain): PointCoordinatesEntity {
+    return PointCoordinatesEntity(point.pointId, point.x, point.y)
 }
--- app/src/main/java/com/example/gh_coursework/data/database/mapper/PointEntityToDomain.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/mapper/PointEntityToDomain.kt
@@ -2,9 +2,7 @@ package com.example.gh_coursework.data.database.mapper
 
 import com.example.gh_coursework.data.database.entity.PointCoordinatesEntity
 import com.example.gh_coursework.domain.entity.PointPreviewDomain
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
-fun mapPointEntityToDomain(point: Flow<PointCoordinatesEntity>): Flow<PointPreviewDomain> {
-        return point.map { PointPreviewDomain(it.pointId, it.x, it.y) }
+fun mapPointEntityToDomain(point: PointCoordinatesEntity): PointPreviewDomain {
+        return  PointPreviewDomain(point.pointId, point.x, point.y)
 }
