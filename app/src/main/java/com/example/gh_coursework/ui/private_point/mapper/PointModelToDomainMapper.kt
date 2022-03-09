aa751ed9106482afcdff8e778ec19451cbefc37d
bogdanboiko <bogdanboiko19996@gmail.com>
2022-03-09 18:54:57 +0200

    /feature_point_db create point ui model and mappers

---
+++ app/src/main/java/com/example/gh_coursework/ui/private_point/mapper/PointModelToDomainMapper.kt
@@ -0,0 +1,8 @@
+package com.example.gh_coursework.ui.private_point.mapper
+
+import com.example.gh_coursework.domain.entity.PointPreviewDomain
+import com.example.gh_coursework.ui.private_point.model.PrivatePointModel
+
+fun mapPointModelToDomain(point: PrivatePointModel): PointPreviewDomain {
+    return PointPreviewDomain(point.pointId, point.x, point.y)
+}
