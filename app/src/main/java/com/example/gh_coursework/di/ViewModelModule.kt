c1ca83b32406880a9f220648466ad3ef257cac65
Pavel Shakin <pashkin302@gmail.com>
2022-03-10 20:45:00 +0200

    /feature_routes_db Added RouteViewModel to di ViewModelModule.kt

--- app/src/main/java/com/example/gh_coursework/di/ViewModelModule.kt
+++ app/src/main/java/com/example/gh_coursework/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.example.gh_coursework.di
 
 import com.example.gh_coursework.ui.point_details.PointDetailsViewModel
 import com.example.gh_coursework.ui.private_point.PointViewModel
+import com.example.gh_coursework.ui.private_route.RouteViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
@@ -9,6 +10,10 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel {
         PointViewModel(get(), get(), get(), get())
+    }
+
+    viewModel {
+        RouteViewModel(get(), get(), get(), get())
     }
 
     viewModel {
