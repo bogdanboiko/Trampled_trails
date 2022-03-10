1e8829558ca3467781a402bc4cdffd27656533d3
bogdanboiko <bogdanboiko19996@gmail.com>
2022-03-10 17:43:06 +0200

    /feature_point_details_db add foreign kay for cascade delete

--- app/src/main/java/com/example/gh_coursework/data/database/entity/PointDetailsEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/entity/PointDetailsEntity.kt
@@ -2,9 +2,15 @@ package com.example.gh_coursework.data.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "point_details")
+@Entity(
+    tableName = "point_details", foreignKeys = [ForeignKey(entity = PointCoordinatesEntity::class,
+        onDelete = CASCADE,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("pointId"))])
 data class PointDetailsEntity(
     @PrimaryKey
     @ColumnInfo(name = "pointId")
