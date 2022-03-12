d5ae3caabf526eb148c2a629692c05e1a8d06e2d
Pavel Shakin <pashkin302@gmail.com>
2022-03-12 23:43:04 +0200

    /feature_routes_db Create point domain-model and GeoJsonPoint-model, route domain-model mappers

---
+++ app/src/main/java/com/example/gh_coursework/ui/private_route/mapper/RouteDomainToModelMapper.kt
@@ -0,0 +1,15 @@
+package com.example.gh_coursework.ui.private_route.mapper
+
+import com.example.gh_coursework.domain.entity.RouteDomain
+import com.example.gh_coursework.ui.private_route.model.PrivateRouteModel
+import com.example.gh_coursework.ui.private_route.model.PrivateRoutePointModel
+
+fun mapRouteDomainToModel(routeDomain: RouteDomain): PrivateRouteModel {
+    val pointCoordinatesModelList = mutableListOf<PrivateRoutePointModel>()
+
+    routeDomain.coordinatesList.forEach {
+        pointCoordinatesModelList.add(mapRoutePointDomainToModel(it))
+    }
+
+    return PrivateRouteModel(routeDomain.routeId, pointCoordinatesModelList)
+}
---
+++ app/src/main/java/com/example/gh_coursework/ui/private_route/mapper/RouteModelToDomainMapper.kt
@@ -0,0 +1,17 @@
+package com.example.gh_coursework.ui.private_route.mapper
+
+import com.example.gh_coursework.domain.entity.PointPreviewDomain
+import com.example.gh_coursework.domain.entity.RouteDomain
+import com.example.gh_coursework.ui.private_point.mapper.mapPointModelToDomain
+import com.example.gh_coursework.ui.private_route.model.PrivateRouteModel
+import com.mapbox.geojson.Point
+
+fun mapRouteModelToDomainMapper(route: PrivateRouteModel): RouteDomain {
+    val pointCoordinatesDomainList = mutableListOf<PointPreviewDomain>()
+
+    route.coordinatesList.forEach {
+        pointCoordinatesDomainList.add(mapRoutePointModelToDomain(it))
+    }
+
+    return RouteDomain(route.routeId, "test", "test", null, pointCoordinatesDomainList)
+}
