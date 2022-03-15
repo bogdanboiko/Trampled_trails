9ae46032866013da62fe02cdcd7ed65ced83abcf
Pavel Shakin <pashkin302@gmail.com>
2022-03-15 20:47:58 +0200

    /feature_routes_details Modify di UseCaseModule.kt and ViewModelModule.kt

--- app/src/main/java/com/example/gh_coursework/di/UseCaseModule.kt
+++ app/src/main/java/com/example/gh_coursework/di/UseCaseModule.kt
@@ -1,51 +1,49 @@
 package com.example.gh_coursework.di
 
-import com.example.gh_coursework.domain.usecase.*
+import com.example.gh_coursework.domain.usecase.point_details.AddPointDetailsUseCase
+import com.example.gh_coursework.domain.usecase.point_details.AddPointDetailsUseCaseImpl
+import com.example.gh_coursework.domain.usecase.point_details.GetPointDetailsUseCase
+import com.example.gh_coursework.domain.usecase.point_details.GetPointDetailsUseCaseImpl
+import com.example.gh_coursework.domain.usecase.point_preview.*
+import com.example.gh_coursework.domain.usecase.point_tag.*
+import com.example.gh_coursework.domain.usecase.route.*
 import org.koin.dsl.module
 
 val pointUseCasesModule = module {
-    single<AddPointDetailsUseCase> {
-        AddPointDetailsUseCaseImpl(get())
-    }
 
+    //Point preview
     single<AddPointPreviewUseCase> {
         AddPointPreviewUseCaseImpl(get())
-    }
-
-    single<AddRouteUseCase> {
-        AddRouteUseCaseImpl(get())
-    }
-
-    single<DeletePointUseCase> {
-        DeletePointUseCaseImpl(get())
-    }
-
-    single<GetPointDetailsUseCase> {
-        GetPointDetailsUseCaseImpl(get())
     }
 
     single<GetPointsPreviewUseCase> {
         GetPointsPreviewUseCaseImpl(get())
     }
 
-    single<GetRoutesListUseCase> {
-        GetRoutesListUseCaseImpl(get())
+    single<DeletePointUseCase> {
+        DeletePointUseCaseImpl(get())
     }
 
-    single<GetRouteUseCase> {
-        GetRouteUseCaseImpl(get())
+    //Point details
+    single<AddPointDetailsUseCase> {
+        AddPointDetailsUseCaseImpl(get())
     }
 
+    single<GetPointDetailsUseCase> {
+        GetPointDetailsUseCaseImpl(get())
+    }
+
+    //Point tag
     single<AddPointTagUseCase> {
         AddPointTagUseCaseImpl(get())
     }
 
-    single<GetPointTagListUseCase> {
-        GetPointTagListUseCaseImpl(get())
-    }
-
     single<AddPointsTagsListUseCase> {
         AddPointsTagsListUseCaseImpl(get())
+    }
+
+    single<GetPointTagListUseCase> {
+        GetPointTagListUseCaseImpl(get())
     }
 
     single<RemovePointsTagsListUseCase> {
@@ -54,5 +52,22 @@ val pointUseCasesModule = module {
 
     single<DeletePointTagUseCase> {
         DeletePointTagUseCaseImpl(get())
+    }
+
+    //Routes
+    single<AddRouteUseCase> {
+        AddRouteUseCaseImpl(get())
+    }
+
+    single<GetRoutesListUseCase> {
+        GetRoutesListUseCaseImpl(get())
+    }
+
+    single<GetRoutePointsUseCase> {
+        GetRoutePointsUseCaseImpl(get())
+    }
+
+    single<DeleteRouteUseCase> {
+        DeleteRouteUseCaseImpl(get())
     }
 }
