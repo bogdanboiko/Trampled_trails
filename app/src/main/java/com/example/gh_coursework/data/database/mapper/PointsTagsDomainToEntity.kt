c1c010b6fe2e8c72def7de5c79d5e31cf4954579
Pavel Shakin <pashkin302@gmail.com>
2022-03-16 17:39:54 +0200

    /feature_routes_details Format lines

--- app/src/main/java/com/example/gh_coursework/data/database/mapper/PointsTagsDomainToEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/mapper/PointsTagsDomainToEntity.kt
@@ -3,6 +3,6 @@ package com.example.gh_coursework.data.database.mapper
 import com.example.gh_coursework.data.database.entity.PointsTagsEntity
 import com.example.gh_coursework.domain.entity.PointsTagsDomain
 
-fun mapPointsTagsDomainToEntity(pointsTags: PointsTagsDomain) : PointsTagsEntity {
+fun mapPointsTagsDomainToEntity(pointsTags: PointsTagsDomain): PointsTagsEntity {
     return PointsTagsEntity(pointsTags.pointId, pointsTags.tagId)
 }
