353e9bbe795ada73d0fd136b952905d30ffb9c04
Pavel Shakin <pashkin302@gmail.com>
2022-03-16 17:41:25 +0200

    /feature_routes_details DataBaseModule.kt create route callback sample

--- app/src/main/java/com/example/gh_coursework/di/DataBaseModule.kt
+++ app/src/main/java/com/example/gh_coursework/di/DataBaseModule.kt
@@ -1,12 +1,26 @@
 package com.example.gh_coursework.di
 
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.gh_coursework.data.database.TravelDatabase
 import org.koin.dsl.module
 
 val localDataBaseModule = module {
     single {
         Room.databaseBuilder(get(), TravelDatabase::class.java, "Travel.db")
+//            .addCallback(object : RoomDatabase.Callback() {
+//                override fun onCreate(db: SupportSQLiteDatabase) {
+//                    super.onCreate(db)
+//                    db.execSQL(
+//                        """
+//                            CREATE TRIGGER IF NOT EXISTS route_details.delete_route
+//                            BEFORE DELETE ON route_details
+//                            BEGIN DELETE FROM routes_points WHERE routeId = DELETED.routeId; END
+//                        """.trimIndent()
+//                    )
+//                }
+//            })
             .fallbackToDestructiveMigration()
             .build()
     }
