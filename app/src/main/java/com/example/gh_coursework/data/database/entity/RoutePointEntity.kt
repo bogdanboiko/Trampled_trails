331efd634d0a8f1698bd8b7df05a5136169fb69d
Pavel Shakin <pashkin302@gmail.com>
2022-03-16 17:32:10 +0200

    /feature_routes_details RoutePointEntity.kt add foreign key

--- app/src/main/java/com/example/gh_coursework/data/database/entity/RoutePointEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/entity/RoutePointEntity.kt
@@ -2,9 +2,18 @@ package com.example.gh_coursework.data.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "routes_points")
+@Entity(
+    tableName = "routes_points", foreignKeys = [ForeignKey(
+        entity = PointCoordinatesEntity::class,
+        onDelete = CASCADE,
+        parentColumns = arrayOf("pointId"),
+        childColumns = arrayOf("pointId")
+    )]
+)
 data class RoutePointEntity(
     @ColumnInfo(name = "routeId")
     var routeId: Long?,
