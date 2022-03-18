9e72b3a82b76e34f5c3fc94e461e428bd8207d99
Pavel Shakin <pashkin302@gmail.com>
2022-03-18 20:00:01 +0200

    /fix_routes delete useless PointToPrivateRoutePointModel.kt mapper

--- app/src/main/java/com/example/gh_coursework/ui/private_route/mapper/PointToPrivateRoutePointModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gh_coursework.ui.private_route.mapper
-
-import com.example.gh_coursework.ui.private_route.model.PrivateRoutePointModel
-import com.mapbox.geojson.Point
-
-fun mapPointToPrivateRoutePointModel(point: Point): PrivateRoutePointModel {
-    return PrivateRoutePointModel(null, point.longitude(), point.latitude(), true)
-}
