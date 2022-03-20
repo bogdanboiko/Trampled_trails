3bd0452f76a9eeb01a33b8394249b89f5fe5f035
bogdanboiko <bogdanboiko19996@gmail.com>
2022-03-20 19:37:08 +0200

    /feature_images add local images fetching

--- app/src/main/java/com/example/gh_coursework/ui/point_details/PointDetailsFragment.kt
+++ app/src/main/java/com/example/gh_coursework/ui/point_details/PointDetailsFragment.kt
@@ -1,9 +1,18 @@
 package com.example.gh_coursework.ui.point_details
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -16,6 +25,25 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
 class PointDetailsFragment : Fragment(R.layout.fragment_point_details) {
+    private val imageTakerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                if (data?.clipData == null) {
+                    val imageUri = data?.data
+
+                    if (imageUri != null) {
+                        val inStream = activity?.contentResolver?.openInputStream(imageUri)
+                        inStream.use {
+                            val image = Drawable.createFromStream(inStream, imageUri.toString())
+                            binding.pointImage.setImageBitmap(image.toBitmap())
+                        }
+                    }
+                } else {
+                    val imageList = data.clipData
+                }
+            }
+        }
     private val arguments by navArgs<PointDetailsFragmentArgs>()
     private val viewModel: PointDetailsViewModel by viewModel { parametersOf(arguments.pointId) }
     private lateinit var binding: FragmentPointDetailsBinding
@@ -92,7 +120,11 @@ class PointDetailsFragment : Fragment(R.layout.fragment_point_details) {
             }
 
             pointImageAddButton.setOnClickListener {
-
+                val transitionToGallery = Intent()
+                transitionToGallery.type = "image/*"
+                transitionToGallery.action = Intent.ACTION_GET_CONTENT
+                transitionToGallery.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                imageTakerLauncher.launch(Intent.createChooser(transitionToGallery, "Select pictures"))
             }
         }
     }
