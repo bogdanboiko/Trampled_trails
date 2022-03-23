616e50bacc679b4dbc0505050bc76613aa61221d
Pavel Shakin <pashkin302@gmail.com>
2022-03-23 14:57:31 +0200

    /fix_routes MyApplication.kt formatted lines

--- app/src/main/java/com/example/gh_coursework/MyApplication.kt
+++ app/src/main/java/com/example/gh_coursework/MyApplication.kt
@@ -6,8 +6,10 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class MyApplication: Application() {
+
     override fun onCreate() {
         super.onCreate()
+
         startKoin {
             androidContext(this@MyApplication)
             modules(ModuleList)
