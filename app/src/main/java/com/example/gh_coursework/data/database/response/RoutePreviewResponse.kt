5823a6ee5378d6949fd115e44dfdec5445be5336
Pavel Shakin <pashkin302@gmail.com>
2022-03-30 21:53:49 +0300

    /feature_route_details_images Create response models for RoutePreviewResponse.kt and RoutePointsResponse.kt

--- app/src/main/java/com/example/gh_coursework/data/database/response/RoutePreviewResponse.kt
+++ app/src/main/java/com/example/gh_coursework/data/database/response/RoutePreviewResponse.kt
@@ -3,25 +3,22 @@ package com.example.gh_coursework.data.database.response
 import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
-import com.example.gh_coursework.data.database.entity.PointCoordinatesEntity
-import com.example.gh_coursework.data.database.entity.RouteEntity
-import com.example.gh_coursework.data.database.entity.RoutePointEntity
+import com.example.gh_coursework.data.database.entity.*
 
 data class RoutePreviewResponse(
     @Embedded
     val route: RouteEntity,
-    @Embedded
-    val details: RouteDetailsResponse,
+
     @Relation(
         parentColumn = "routeId",
-        entityColumn = "pointId",
-        associateBy = Junction(RoutePointEntity::class)
+        entityColumn = "tagId",
+        associateBy = Junction(RouteTagsEntity::class)
     )
-    val points: List<PointDetailsResponse>,
+    val tagList: List<RouteTagEntity>,
+
     @Relation(
         parentColumn = "routeId",
-        entityColumn = "pointId",
-        associateBy = Junction(RoutePointEntity::class)
+        entityColumn = "routeId",
     )
-    val coordinates: List<PointCoordinatesEntity>
+    val imageList: List<RouteImageEntity>
 )
