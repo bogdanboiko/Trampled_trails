4b77f1320adb1e2ca70644805aff7abf30c1ea44
Pavel Shakin <pashkin302@gmail.com>
2022-04-06 17:42:05 +0300

    /feature_firebase_auth Added homepage button xml and added it to fragment layouts

---
+++ app/src/main/java/com/example/gh_coursework/ui/home/HomepageFragment.kt
@@ -0,0 +1,28 @@
+package com.example.gh_coursework.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.gh_coursework.databinding.FragmentHomepageBinding
+
+class HomepageFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomepageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomepageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
