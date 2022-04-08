5fb7f494b12cb25a30433ec65b6b19cdfb7aa2e0
bogdanboiko <bogdanboiko19996@gmail.com>
2022-04-08 12:57:11 +0300

    /feature_route_saving_to_firebase add route images saving to storage

--- app/src/main/java/com/example/gh_coursework/data/remote/RemoteDataSrcImpl.kt
+++ app/src/main/java/com/example/gh_coursework/data/remote/RemoteDataSrcImpl.kt
@@ -1,6 +1,8 @@
 package com.example.gh_coursework.data.remote
 
+import android.net.Uri
 import android.util.Log
+import androidx.core.net.toFile
 import com.example.gh_coursework.data.datasource.TravelDatasource
 import com.example.gh_coursework.data.remote.mapper.mapRouteDomainToPublicRouteEntity
 import com.example.gh_coursework.data.remote.mapper.mapRoutePointDomainToPublicRoutePointEntity
@@ -8,17 +10,31 @@ import com.example.gh_coursework.domain.entity.RouteDomain
 import com.example.gh_coursework.domain.entity.RoutePointDomain
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class RemoteDataSrcImpl : TravelDatasource.Remote {
     private val db = Firebase.firestore
+    private val storage = FirebaseStorage.getInstance("gs://gh-coursework.appspot.com")
+    private val ref = storage.reference
+
     override fun publishRoute(route: RouteDomain, routePoints: List<RoutePointDomain>) {
-        Log.e("e", route.imageList.toString())
+        val imageUrls = mutableListOf<String>()
+
+        route.imageList.forEach { imageUrl ->
+            val generatedImageId = UUID.randomUUID().toString()
+            ref.child("route_images/$generatedImageId")
+                .putFile(Uri.parse(imageUrl.image)).addOnSuccessListener {
+                    imageUrls.add(generatedImageId)
+                }
+        }
+
         db.collection("routes")
-            .add(mapRouteDomainToPublicRouteEntity(route))
-            .addOnSuccessListener {
+            .add(mapRouteDomainToPublicRouteEntity(route, imageUrls))
+            .addOnSuccessListener { routeDocument ->
                 routePoints.forEach { routePoint ->
                     db.collection("routes")
-                        .document(it.id)
+                        .document(routeDocument.id)
                         .collection("points")
                         .add(mapRoutePointDomainToPublicRoutePointEntity(routePoint))
                 }
--- app/src/main/java/com/example/gh_coursework/data/remote/mapper/RouteDomainToPublicRouteEntity.kt
+++ app/src/main/java/com/example/gh_coursework/data/remote/mapper/RouteDomainToPublicRouteEntity.kt
@@ -3,10 +3,10 @@ package com.example.gh_coursework.data.remote.mapper
 import com.example.gh_coursework.data.remote.entity.PublicRouteEntity
 import com.example.gh_coursework.domain.entity.RouteDomain
 
-fun mapRouteDomainToPublicRouteEntity(route: RouteDomain): PublicRouteEntity {
+fun mapRouteDomainToPublicRouteEntity(route: RouteDomain, imageList: List<String>): PublicRouteEntity {
     return PublicRouteEntity(
         route.name,
         route.description,
         route.tagsList.map { it.name },
-        route.imageList.map { it.image })
+        imageList)
 }
