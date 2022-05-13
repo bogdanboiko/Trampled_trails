62f2a803d50bff98d90035ee82d26cc507a8b2d5
Pavel Shakin <pashkin302@gmail.com>
2022-05-13 17:49:08 +0300

    /fix_refactor Added to AnnotationHelper.kt erase camera function

--- app/src/main/java/com/example/gh_coursework/ui/helper/AnnotationHelper.kt
+++ app/src/main/java/com/example/gh_coursework/ui/helper/AnnotationHelper.kt
@@ -5,7 +5,10 @@ import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
 import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
+import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 
 fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
@@ -41,4 +44,13 @@ fun createFlagAnnotationPoint(bitmap: Bitmap, point: Point): PointAnnotationOpti
         .withIconImage(bitmap)
         .withIconAnchor(IconAnchor.BOTTOM_LEFT)
         .withIconOffset(listOf(-9.6, 3.8))
+}
+
+fun eraseCameraToPoint(x: Double, y: Double, mapView: MapView) {
+    mapView.camera.easeTo(
+        CameraOptions.Builder()
+            .center(Point.fromLngLat(x, y))
+            .zoom(14.0)
+            .build()
+    )
 }
